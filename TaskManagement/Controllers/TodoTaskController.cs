6f4bfb5f96b95b9a631061ccd4b7dfe7a1a67167
gideon.edoghotu <gideon.edoghotu@cyphercrescent.com>
2025-08-14 15:27:26 +0100

    added the controller implementations

---
+++ TaskManagement/Controllers/TodoTaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoTaskController : ControllerBase
+    {
+        // Create Operation
+
+
+
+        // Fetch By Id Operation
+
+
+
+        // Fetch All Operation
+
+
+
+        // Update Operation
+
+
+        // Delete Operation
+    }
+}
---
+++ TaskManagement/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // Create Operation
+
+
+
+        // Fetch By Id Operation
+
+
+
+        // Fetch All Operation
+
+
+
+        // Update Operation
+
+
+        // Delete Operation
+
+    }
+}
