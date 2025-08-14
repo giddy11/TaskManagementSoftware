7e1b28766690946efba60ac8d60e3ddf6d14abd4
gideon.edoghotu <gideon.edoghotu@cyphercrescent.com>
2025-08-14 15:46:58 +0100

    updated

--- TaskManagement/Controllers/UserController.cs
+++ TaskManagement/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.UserManagement;
+using TaskManagement.Persistence;
 
 namespace TaskManagement.API.Controllers
 {
@@ -7,22 +9,98 @@ namespace TaskManagement.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly TaskManagementDbContext _context;
+
+        public UserController(TaskManagementDbContext context)
+        {
+            _context = context;
+        }
+
         // Create Operation
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
 
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
 
         // Fetch By Id Operation
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
 
+            if (user == null)
+            {
+                return NotFound();
+            }
 
+            return user;
+        }
 
         // Fetch All Operation
-
-
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
 
         // Update Operation
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
 
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
 
         // Delete Operation
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
     }
 }
