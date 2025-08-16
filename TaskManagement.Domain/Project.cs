2428885516376784c4f5ca5cb99cc23582f1b718
Precious Steve <precioussteve01@gmail.com>
2025-08-16 13:33:40 +0100

    add project class

---
+++ TaskManagement.Domain/Project.cs
@@ -0,0 +1,16 @@
+﻿using TaskManagement.Domain.UserManagement;
+
+namespace TaskManagement.Domain
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = default!;
+        public string? Description { get; set; }
+        public int CreatedById { get; set; }
+        public User CreatedBy { get; set; } = default!;
+        public string? Version { get; set; }
+        public string? Version { get; set; }
+        public string? Version { get; set; }
+    }
+}
